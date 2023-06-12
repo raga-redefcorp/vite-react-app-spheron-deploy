7a7c7586503112344bdbd7e8a8ac958f76223d6c
raga-redefcorp <raga@redefcorp.in>
2023-06-12 12:37:45 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,6 @@ const App = () => {
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
